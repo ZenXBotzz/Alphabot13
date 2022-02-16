a95c0e3e00ea74501646e29a9f6839e1ba6dcfe1
ErozeBotz <rokhis346@gmail.com>
2022-02-16 19:23:48 +0700

    Alphabot15

---
+++ shop/grupbot.js
@@ -0,0 +1,18 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+
+1. *ErozeBotz Support*
+_https://chat.whatsapp.com/LNSLUbCXb7S6mGFBFITY76_
+2. *Alphabot Support 2*
+_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
+3. *Alphabot Support 3*
+_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
+4. *Alphabot Support 4* 
+_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
---
+++ shop/sewa.js
@@ -0,0 +1,24 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 3.000
+⬡ 1 BULAN = Rp 5.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 25K
+
+
+⬣ MINAT? PM
+⬡ wa.me/62895326739863
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
